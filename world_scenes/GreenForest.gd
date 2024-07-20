9edb3ac2ed494f7b341d04b726a3979bc156b672
Jake012345 <nagyerik012345@gmail.com>
2024-07-21 00:42:51 +0200

    -Moved Game.gd to scripts folder -Started planning the data structure of world generator's input

--- world_scenes/GreenForest.gd
+++ world_scenes/GreenForest.gd
@@ -4,6 +4,37 @@ var enemy_pumpkin_root: PackedScene = load("res://classes/enemy_pumpkin_basic.ts
 var spawn_point: Vector2 = Vector2.ZERO
 
 
+""" GENERATOR DATA
+
+var x: Dictionary = 
+{
+   "map_size_x": 10,
+   "map_size_y": 10,
+   "is_map_bordered": true,
+   "default_map_border": "res://16x16_single_tile.png"
+   "areas": {
+      area_name_1: {
+         "difficulty": 1
+         "rarity": 1
+         "is_bordered": true
+         "borders": {             <--- only if IS_BORDERED
+            "right": "res://16x16_single_tile.png"
+            "left": "res://16x16_single_tile.png"
+            "top": "res://16x16_single_tile.png"
+            "bottom": "res://16x16_single_tile.png"
+           }
+         "tiles": {                    the tiles will have the relative rarity of 1:3:5
+            1: "res://16x16_single_tile_png"
+            3: "res://16x16_single_tile_png"
+            5: "res://16x16_single_tile_png"
+           }
+        },
+      area_name_2:{}
+     }
+  }
+
+"""
+
 func _physics_process(delta):
    
    pass
