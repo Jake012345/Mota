308d91c20bb64533db64f8e68f8228ade69ec2f5
Jake012345 <nagyerik012345@gmail.com>
2024-07-26 21:19:31 +0200

    Started rewriting world autotile + bitmap to be compatible with later map generation concept
    Signed-off-by: Jake012345 <nagyerik012345@gmail.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Mota
+
+**Mota is a tile-based, pixel art, multiplayer dungeon crawler game.**
+
+Similarly to [Terraira](https://terraria.org/) user profiles contain the character data (items, stats) and all maps are clean generated every time a server starts. 
+
+#### Supported network
+- LAN/WAN direct connect
+- LAN server discovery
+
+#### Supported modes
+(soon)
+
+#### Creation
+The game was first created in 2024 being made with [Pandemonium](https://github.com/Relintai/pandemonium_engine/) game engine, that is a subversion of [Godot](https://godotengine.org/) game engine.
--- world_scenes/GreenForest.gd
+++ world_scenes/GreenForest.gd
@@ -3,30 +3,43 @@ extends Node2D
 var enemy_pumpkin_root: PackedScene = load("res://classes/enemy_pumpkin_basic.tscn")
 var spawn_point: Vector2 = Vector2.ZERO
 
+export var generator_params: Dictionary = {
+   "map_size_x": 10,
+   "map_size_y": 10,
+   "is_map_bordered": true,
+   "default_border": {
+      
+     }
+  }
 
 """ GENERATOR DATA
 
-var x: Dictionary = 
+var generator_params: Dictionary = 
 {
    "map_size_x": 10,
    "map_size_y": 10,
    "is_map_bordered": true,
-   "default_map_border": "res://16x16_single_tile.png"
+   "default_map_border":  {
+      "right":"res://16x16_single_tile.png"
+      "left":"res://16x16_single_tile.png"
+      "top":"res://16x16_single_tile.png"
+      "bottom":"res://16x16_single_tile.png"
+      }
    "areas": {
       area_name_1: {
-         "difficulty": 1
-         "rarity": 1
-         "is_bordered": true
+         "difficulty": 1,
+         "rarity": 1,
+         "is_bordered": false,
          "borders": {             <--- only if IS_BORDERED
-            "right": "res://16x16_single_tile.png"
-            "left": "res://16x16_single_tile.png"
-            "top": "res://16x16_single_tile.png"
-            "bottom": "res://16x16_single_tile.png"
+            "right": "res://16x16_single_tile.png",
+            "left": "res://16x16_single_tile.png",
+            "top": "res://16x16_single_tile.png",
+            "bottom": "res://16x16_single_tile.png",
            }
          "tiles": {                    the tiles will have the relative rarity of 1:3:5
-            1: "res://16x16_single_tile_png"
-            3: "res://16x16_single_tile_png"
-            5: "res://16x16_single_tile_png"
+            1: "res://16x16_single_tile_png",
+            3: "res://16x16_single_tile_png",
+            5: "res://16x16_single_tile_png",
            }
         },
       area_name_2:{}
